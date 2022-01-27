570c157f02a4c8acf2286707d46c4881a1dc5918
RoyerMateu <rogermateu2001@gmail.com>
2022-01-27 09:39:53 +0100

    htacces+constants+db

---
+++ config/constants.php
@@ -0,0 +1,19 @@
+<?php
+//LIBS
+define("LIBS", BASE_PATH . '/libs');
+
+//CONTROLLERS
+define("CONTROLLERS", BASE_PATH . '/controllers');
+
+//VIEWS
+define("VIEWS", BASE_PATH . '/views');
+
+//MODELS
+define("MODELS", BASE_PATH . '/models');
+
+//JS
+define('JS', BASE_URL . '/assets/js');
+
+//HEADER & FOOTER
+define('HTML', BASE_PATH . '/assets/html')
+?>
---
+++ db/db.sql
@@ -0,0 +1,12 @@
+--
+-- Database creation
+--
+
+DROP DATABASE IF EXISTS employee_management_v2;
+CREATE DATABASE employee_management_v2;
+USE employee_management_v2;
+
+--
+-- Tables creation
+--
+
