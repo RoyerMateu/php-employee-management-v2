5749b3beb626e7a9e911efbb015d4a93c755643b
RoyerMateu <rogermateu2001@gmail.com>
2022-01-28 10:45:26 +0100

    delete

--- src/imageGallery.php
+++
@@ -1,6 +0,0 @@
-<!-- TODO If you are going to add the extra feature implement here the image selection as a gallery or whatever you like -->
-
-
-<div id="avatarsCarrousel" class="avatarsCarrousel">
-
-</div>
